0fe102bb5dd24981a58d68571841affa1dea9355
Bryan Wilhite <rasx@songhaysystem.com>
2018-05-18 15:49:32 -0700

    :pencil: tidying?

--- Songhay.Blog/Startup.cs
+++ Songhay.Blog/Startup.cs
@@ -76,28 +76,26 @@ namespace Songhay.Blog
                 app.UseStatusCodePages();
             }
 
-            app.UseStaticFiles();
             app.UseSpaStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
+            app
+                .UseMvc(routes =>
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller}/{action=Index}/{id?}");
+                })
+                .UseSpa(spa =>
+                {
+                    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                    // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                    spa.Options.SourcePath = "ClientApp";
+
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseAngularCliServer(npmScript: "start");
+                    }
+                });
         }
     }
 }
