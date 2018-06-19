86faa1acea2ff5810e92a025cf231737187acf46
Bryan Wilhite <rasx@songhaysystem.com>
2018-06-19 10:11:30 -0700

    :lipstick: added trusted content binding

--- Songhay.Blog/ClientApp/src/app/components/app-blog-entry/app-blog-entry.component.ts
+++ Songhay.Blog/ClientApp/src/app/components/app-blog-entry/app-blog-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 import { BlogEntriesService } from '../../services/songhay-blog-entries.service';
@@ -11,14 +12,25 @@ import { BlogEntriesService } from '../../services/songhay-blog-entries.service'
 export class AppBlogEntryComponent implements OnInit {
     constructor(
         public indexService: BlogEntriesService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private sanitizer: DomSanitizer
     ) {}
 
     private slug: string;
+    private trustedContent: SafeHtml;
+    private trustedTitle: SafeHtml;
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             this.slug = params['slug'] as string;
+            this.indexService.loadEntry(this.slug).then(() => {
+                this.trustedContent = this.sanitizer.bypassSecurityTrustHtml(
+                    this.indexService.entry.content
+                );
+                this.trustedTitle = this.sanitizer.bypassSecurityTrustHtml(
+                    this.indexService.entry.title
+                );
+            });
         });
     }
 }
