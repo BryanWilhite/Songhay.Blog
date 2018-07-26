af182d2d18057da70c6bfdf6e7ced2513cba1d6f
Bryan Wilhite <rasx@songhaysystem.com>
2018-07-25 18:10:28 -0700

    :sparkles: added ConfigurationBuilderExtensions; addressing issue #22

---
+++ Songhay.Blog.Repository/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Songhay.Blog.Models;
+using Songhay.Models;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Songhay.Blog.Repository.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="ConfigurationBuilder"/>
+    /// </summary>
+    public static class ConfigurationBuilderExtensions
+    {
+        public static CloudStorageAccount ToCloudStorageAccount(this ConfigurationBuilder builder, string basePath)
+        {
+            if (builder == null) throw new NullReferenceException("The expected Configuration builder is not here.");
+            if (!Directory.Exists(basePath)) throw new DirectoryNotFoundException("The expected configuration base path is not here.");
+
+            builder
+                .SetBasePath(basePath)
+                .AddJsonFile(AppScalars.conventionalSettingsFile, optional: false, reloadOnChange: true);
+
+            var meta = new ProgramMetadata();
+            builder.Build().Bind(nameof(ProgramMetadata), meta);
+
+            if (meta.CloudStorageSet == null) throw new NullReferenceException("The expected cloud storage set is not here.");
+
+            var key = "SonghayCloudStorage";
+            var test = meta.CloudStorageSet.TryGetValue(key, out var set);
+            if (!test) throw new NullReferenceException($"The expected cloud storage set, {key}, is not here.");
+            if (!set.Any()) throw new NullReferenceException($"The expected cloud storage set items for {key} are not here.");
+
+            var connectionString = set.First().Value;
+            var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+            return cloudStorageAccount;
+        }
+    }
+}
