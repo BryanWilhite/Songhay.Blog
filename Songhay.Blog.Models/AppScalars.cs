ead574521e5cd6cdfdf60d8b0f1a6ab644b39de4
Bryan Wilhite <rasx@songhaysystem.com>
2018-08-02 09:27:37 -0700

    :hammer: :pencil: :lipstick: see issue #19

--- Songhay.Blog.Models/AppScalars.cs
+++ Songhay.Blog.Models/AppScalars.cs
@@ -5,6 +5,24 @@
     /// </summary>
     public static class AppScalars
     {
+        /// <summary>
+        /// The conventional settings file
+        /// </summary>
         public const string conventionalSettingsFile = "app-settings.songhay-system.json";
+
+        /// <summary>
+        /// The render section blog headers
+        /// </summary>
+        public const string renderSectionBlogHeaders = "BlogHeaders";
+
+        /// <summary>
+        /// The render section social headers
+        /// </summary>
+        public const string renderSectionSocialHeaders = "SocialHeaders";
+
+        /// <summary>
+        /// The render section style headers
+        /// </summary>
+        public const string renderSectionStyleBlock = "StyleBlock";
     }
 }
