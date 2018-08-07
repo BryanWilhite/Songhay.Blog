615297fc29fc9e9e966a52a51db83ac50351aa71
Bryan Wilhite <rasx@songhaysystem.com>
2018-08-07 14:17:42 -0700

    :microscope: addressing #20

---
+++ Songhay.Blog.Shell.Tests/ProgramTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Songhay.Extensions;
+using Songhay.Models;
+using System.IO;
+
+namespace Songhay.Blog.Shell.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [Ignore("This test is meant to run manually on the Desktop.")]
+        [TestMethod]
+        [TestProperty("serverMetadataFile", @"ClientApp\src\assets\data\server-meta.json")]
+        public void ShouldGenerateServerMetadata()
+        {
+            var webProjectInfo = this.TestContext.ShouldGetConventionalProjectDirectoryInfo(this.GetType());
+
+            #region test properties:
+
+            var serverMetadataFile = this.TestContext.Properties["serverMetadataFile"].ToString();
+            serverMetadataFile = Path.Combine(webProjectInfo.FullName, serverMetadataFile);
+            this.TestContext.ShouldFindFile(serverMetadataFile);
+
+            #endregion
+
+            var jO = JObject.Parse(File.ReadAllText(serverMetadataFile));
+            var assemblyInfo = new FrameworkAssemblyInfo(typeof(Program).Assembly);
+
+            this.TestContext.WriteLine($"assemblyInfo: {assemblyInfo}");
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+
+            jO[nameof(assemblyInfo)] = JObject.FromObject(assemblyInfo, JsonSerializer.Create(settings));
+
+            File.WriteAllText(serverMetadataFile, jO.ToString());
+        }
+    }
+}
